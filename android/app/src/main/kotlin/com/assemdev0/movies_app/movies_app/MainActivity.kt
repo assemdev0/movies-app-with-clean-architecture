9ccefa20e6ff2a74b8f03e5c8254716fea93e198
Assem Ashraf <assemdev0@gmail.com>
2023-07-28 08:04:48 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/assemdev0/movies_app/movies_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.assemdev0.movies_app.movies_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/core/utilities/constance.dart
@@ -0,0 +1,4 @@
+class AppConstance {
+  static const String baseUrl = "https://api.themoviedb.org/3";
+  static const String apiKey = "5efb56a35aab9a710a1f08812daa0a81";
+}
